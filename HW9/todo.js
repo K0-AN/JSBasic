9bd9991ccca9689636c962505e5f8aee1595d59d
K0-AN <72803262+K0-AN@users.noreply.github.com>
2020-11-09 19:09:29 +0200

    finished HW10

--- HW9/todo.js
+++ HW9/todo.js
@@ -1,104 +1,112 @@
 'use strict';
 
 
-let length = 5;
+let len = 20;
 let index = 0;
-let todos = {};
+let todos = [];
 
-fetch ('https://jsonplaceholder.typicode.com/todos')
-.then(response => response.json())
-.then(json =>  setTodos(json)); 
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json => setTodos(json));
 
 
 const body = document.getElementById('todos');
 
 function setTodos(json) {
-   todos = json;
-fillTodos();
+  todos = json;
+  fillTodos();
 }
 
-function fillTodos(){
-for (index; index < length; index++) {
-  const addDiv = document.createElement('div');
+function fillTodos() {
+  for (index; index < len; index++) {
+    const addDiv = document.createElement('div');
 
-  addDiv.classList.add('todo');
-  addDiv.id = todos[index].id;
-  if (todos[index].completed == true){
-    addDiv.classList.add('completed');
+    addDiv.classList.add('todo');
+    addDiv.id = todos[index].id;
+    if (todos[index].completed == true) {
+      addDiv.classList.add('completed');
+    }
+    body.append(addDiv);
+
+    const addContainer = document.createElement('div');
+
+    addContainer.classList.add('todo-container');
+    addDiv.prepend(addContainer);
+
+    const addTitle = document.createElement('p');
+
+    addTitle.classList.add('title');
+    addTitle.innerText = todos[index].title
+    addContainer.prepend(addTitle);
+
+    const addDeleteIcon = document.createElement('span');
+
+    addDeleteIcon.classList.add('delete-icon');
+    addContainer.append(addDeleteIcon);
+
+    ///////
+
+    const addContainer2 = document.createElement('div');
+
+    addContainer2.classList.add('todo-container');
+    addDiv.append(addContainer2);
+
+    const addUserID = document.createElement('span');
+
+    addUserID.classList.add('user-id');
+    addUserID.innerText = `User #${todos[index].userId}`
+    addContainer2.prepend(addUserID);
+
+    const addStateIcon = document.createElement('span');
+
+    addStateIcon.classList.add('state-icon');
+
+    addContainer2.append(addStateIcon);
+
   }
-  body.append(addDiv);
-
-  const addContainer = document.createElement('div');
-
-  addContainer.classList.add('todo-container');
-  addDiv.prepend(addContainer);
-
-  const addTitle = document.createElement('p');
-
-  addTitle.classList.add('title');
-  addTitle.innerText = todos[index].title
-  addContainer.prepend(addTitle);
-
-  const addDeleteIcon = document.createElement('span');
-
-  addDeleteIcon.classList.add('delete-icon');
-  addContainer.append(addDeleteIcon);
-
-  ///////
-
-  const addContainer2 = document.createElement('div');
-
-  addContainer2.classList.add('todo-container');
-  addDiv.append(addContainer2);
-
-  const addUserID = document.createElement('span');
-
-  addUserID.classList.add('user-id');
-  addUserID.innerText = `User #${todos[index].userId}`
-  addContainer2.prepend(addUserID);
-
-  const addStateIcon = document.createElement('span');
-
-  addStateIcon.classList.add('state-icon');
-  
-  addContainer2.append(addStateIcon);
-
-}
 }
 const todosContainer = document.getElementById('todos');
 
 todosContainer.addEventListener('click', (event) => {
   const target = event.target;
-  if(target.classList.contains('delete-icon')){
+  if (target.classList.contains('delete-icon')) {
     target.closest('.todo').remove();
 
-  } else if(target.classList.contains('state-icon')){
-    
-     //setTodoState(target.closest('.todo').id, false);
-     
-     fillTodos();
+    deleteTodo(target.closest('.todo').id);
+
+  } else if (target.classList.contains('state-icon')) {
+
+    setTodoState(target.closest('.todo').id);
+
+    if (target.closest('.completed')) {
+      target.closest('.todo').classList.remove('completed');;
+    } else {
+      target.closest('.todo').classList.add('completed');;
+    }
   }
-  
+
 })
 
-function setTodoState(id, state) {
-//  for (let i=0; i<todos.length; i++) {
-//    if (todos[i].id == id) {
-//      todos[i].completed = state;
-//    }
-//  }
-//}
-let itemId = todos.find(item => item.id == id);
-console.log(itemId);
-    
+function setTodoState(id) {
+  let itemId = todos.find(item => item.id == id);
+  console.log(itemId);
+  itemId.completed = !itemId.completed;
+  console.log(itemId);
+
 }
-setTodoState (4, false);
+
+function deleteTodo(id) {
+  let itemId = todos.findIndex(item => item.id == id);
+  console.log(todos[itemId]);
+  todos.splice(itemId, 1);
+  console.log(todos[itemId]);
+}
 
 
 
 document.getElementById('loadTodos')
-.onclick = function (event) {
+  .onclick = function (event) {
 
-  length += 1;
-  fillTodos();
-}
+    len += 20;
+    fillTodos();
+  }
